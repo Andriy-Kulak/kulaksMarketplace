27be75562ee6a4cba2703a192e7214184aff84c0
Andriy Kulak <akulak@fuzzproductions.com>
2018-07-06 23:19:39 -0400

    init

---
+++ README.md
@@ -0,0 +1,60 @@
+conduct leaf wall visit acid ask trumpet win country vault pig security
+
+
+Next Goals:
+- Create a few more elaborate contract
+- figure out how to properly store admins (potentially ask on the admin)
+- Test deploying the web app and make sure it points to the deployed contract
+
+commands
+- ganache-cli
+- truffle migrate --reset
+- npm run start
+
+
+Order of operations
+1. truffle unbox react
+2. change truffle.js and getWeb3.js (line 24) files as indicated
+3. run > ganache-cli and copy mneumoic
+4. paste the mneumonic to metamask and go to localhost:8545
+5. run > truffle migrate --reset
+6. npm run start
+
+
+-------- Steps I took
+1. truffle unbox react-uport
+2. go to appmanger.uport.me
+3. create uport account. Instead of signing in you, use your phone to authenticate requests
+
+
+Need to Figure out how to:
+- https://ethereum.stackexchange.com/questions/38865/how-to-deploy-a-truffle-app-in-heroku
+- For the connect uport, make sure to put in on the server somewhere in a lambda function
+- Persist uPort Login in local storage
+
+
+Locally Start
+1. run > ganache-cli and copy mneumoic
+2. paste the mneumonic to metamask and go to localhost:8545
+3. run > truffle migrate --reset
+4. npm run start
+
+Notes
+- deploying with Infura and Truffle: https://truffleframework.com/tutorials/using-infura-custom-provider
+    1. `truffle compile`
+    2. `truffle migrate --network rinkeby`
+- to check your deployed contract:
+    1. Go to remix, (Environment should be Injected Web3) and paste in address and click "at Address"
+
+
+
+
+Casino Contract:
+- https://medium.com/@merunasgrincalaitis/the-ultimate-end-to-end-tutorial-to-create-and-deploy-a-fully-descentralized-dapp-in-ethereum-18f0cf6d7e0e
+
+
+Note About Functions:
+The functions in solidity always return transaction hash. You can use constant functions to return values. The constant functions can not change the state of contract.
+
+If you need to change state of contract in a function and aslo want to return some value, Events are best possible solution AFAIK.
+
---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,96 @@
+pragma solidity ^0.4.17;
+
+contract KulaksMarketplace {
+  struct Product {
+    uint id;
+    string name;
+    string description;
+    uint price;
+    
+  }
+  struct Store {
+    string name;
+    string storeType;
+    string description;
+    address owner;
+    // Product[] products;
+  }
+  uint storedData;
+  mapping(address => bool) public admins;
+  mapping(address => bool) public shopOwners;
+  mapping(address => Store) public stores;
+  address[] private storeOwners;
+  
+  constructor(address creator) public {
+      admins[creator] = true;
+  }
+  
+  modifier shopOwnerOnly() {
+    require(shopOwners[msg.sender] == true);
+    _;
+  }
+  
+  modifier adminOnly() {
+    require(shopOwners[msg.sender] == true);
+    _;
+  }
+  
+  
+  function createStore(string _name, string _storeType, string _description) public shopOwnerOnly {
+     Store memory newStore = Store({
+       name: _name,
+       description: _description,
+       storeType: _storeType,
+       owner: msg.sender
+       // products: new Product[](0)
+    });
+      // need to add a
+     stores[msg.sender] = newStore;
+    // stores.push(newStore);
+  }
+  
+  function getStoreByOwner() public view returns(string, string, string, address) {
+     return (
+        stores[msg.sender].name,
+        stores[msg.sender].description,
+        stores[msg.sender].storeType,
+        stores[msg.sender].owner
+     );
+  }
+
+  function becomeAdmin() public returns (bool) {
+      admins[msg.sender] = true;
+  }
+  
+  function becomeShopOwner() public returns (bool) {
+      shopOwners[msg.sender] = true;
+  }
+  
+  
+  function checkIfSenderAdmin () public view returns (bool) {
+      if(admins[msg.sender] == true) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+  
+  function checkIfSenderShopOwners () public view returns (bool) {
+      if(shopOwners[msg.sender] == true) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+  
+  
+
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
+  }
+}
